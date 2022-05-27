22eb6a1d7fb51c0dff226e989c8d083109b5a7ef
Dxgato <95463027+Dxgato@users.noreply.github.com>
2022-05-27 03:35:54 -0300

    update script.lua

--- assets/preload/data/blazeborn/script.lua
+++ assets/preload/data/blazeborn/script.lua
@@ -3,14 +3,6 @@ local damage = 0;
 local anglevar = 1;
 local starting 
 local allowCountdown = false
-function onStartCountdown()
-	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
-		startVideo('blazborn');
-		allowCountdown = true;
-		return Function_Stop;
-	end
-	return Function_Continue;
-end
 function onTimerCompleted(tag, loops, loopsLeft)
 	if tag == 'startDialogue' then -- Timer completed, play dialogue
 		startDialogue('dialogue', 'breakfast');
